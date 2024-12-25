1e49fe19ea7074ec634170e1d3b5f0365968f462
tobtoht <tob@featherwallet.org>
2024-12-25 14:24:58 +0100

    guix: remove sponge
    `sponge` doesn't appear to be needed here. `sort -o` does not truncate.

--- contrib/guix/libexec/codesign.sh
+++ contrib/guix/libexec/codesign.sh
@@ -110,6 +110,5 @@ mv --no-target-directory "$OUTDIR" "$ACTUAL_OUTDIR" \
         find "$ACTUAL_OUTDIR" -type f
     } | xargs realpath --relative-base="$PWD" \
         | xargs sha256sum \
-        | sort -k2 \
-        | sponge "$ACTUAL_OUTDIR"/SHA256SUMS.part
+        | sort -k2 -o "$ACTUAL_OUTDIR"/SHA256SUMS.part
 )
